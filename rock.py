f7c6342f5a7b21fce1ed2bd3a7aa7ec653d73754
kzawadi <kelvin.vitone@gmail.com>
2023-05-11 14:23:27 +0300

    a simple python server to play rock paper scissor

---
+++ rock.py
@@ -0,0 +1,18 @@
+from flask import Flask, request
+import random
+app = Flask(__name__)
+
+@app.route('/play', methods=['POST'])
+def play():
+    data = request.get_json()
+    client_move = data['move']
+    server_move = random.choice(['rock', 'paper', 'scissors'])
+    if client_move == server_move:
+        result = 'tie'
+    elif client_move == 'rock' and server_move == 'scissors' or \
+            client_move == 'paper' and server_move == 'rock' or \
+            client_move == 'scissors' and server_move == 'paper':
+        result = 'win'
+    else:
+        result = 'lose'
+    return {'result': result, 'server_move': server_move}
